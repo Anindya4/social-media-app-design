1ae6e25fe793594ed23b57517d29b5cf93770c7b
Anindya4 <anindyanandi.work@gmail.com>
2025-07-23 10:47:48 +0530

    commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,104 @@
+import "./sidebar.css"
+import {
+  RssFeed,
+  Chat,
+  PlayCircle,
+  Groups,
+  Bookmark,
+  Help,
+  Work,
+  Event,
+  School,
+} from "@mui/icons-material";
+
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWraper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Chat</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircle className="sidebarIcon" />
+            <span className="sidebarListItemText">Video</span>
+          </li>
+          <li className="sidebarListItem">
+            <Groups className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmark</span>
+          </li>
+          <li className="sidebarListItem">
+            <Help className="sidebarIcon" />
+            <span className="sidebarListItemText">Help</span>
+          </li>
+          <li className="sidebarListItem">
+            <Work className="sidebarIcon" />
+            <span className="sidebarListItemText">Job</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Event</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+        </ul>
+         <button className="sidebarButton">Show More</button>
+         <hr className="sidebarHr"/>
+         <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFrinedName">Alex Kumar</span>
+          </li>
+         </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,7 +8,7 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-          <Search />
+          <Search className="searchIcon" />
           <input
             placeholder="Search for friend, post or videos"
             className="searchInput"
