5304119cabe1f9a0c5d0cdb5d31c6c8c63beda6b
Anindya4 <anindyanandi.work@gmail.com>
2025-07-25 23:56:32 +0530

    profile added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/home/Home";
-
+import Profile from "./pages/profile/Profile";
 
 export default function App() {
-  return <Home />;
+  return <Profile />;
 }
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,17 @@
+import "./profile.css"
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+export default function Profile() {
+  return (
+    <>
+        <Topbar />
+        <div className="homeContainer">
+        <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+        </div>
+    </>
+  )
+}
